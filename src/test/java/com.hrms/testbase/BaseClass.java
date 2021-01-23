c917480a6d44992fff63cc76f75339c6995ff32b
Hdey68 <ouniche.sofiane@gmail.com>
2021-01-22 21:10:37 -0500

    CucumberFramework

--- src/test/java/com.hrms/testbase/BaseClass.java
+++ src/test/java/com.hrms/testbase/BaseClass.java
@@ -12,36 +12,39 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.concurrent.TimeUnit;
 
 public class BaseClass {
-    public static WebDriver driver;
-    /**
-     * this method will open a browser , set up configuration and navigate to url
-     */
-    public static void setUp ( ) {
 
-        ConfigsReader.readProperties ( Constants.CONFIGURATION_FILEPATH );
-        switch (ConfigsReader.getPropertyValue ( "browser" ).toLowerCase ( )) {
+    public static WebDriver driver;
+
+    /**
+     * this method will open a browser, set up configuration and navigate to the URL
+     */
+    public static void setUp() {
+
+        ConfigsReader.readProperties(Constants.CONFIGURATION_FILEPATH);
+        switch (ConfigsReader.getPropertyValue("browser").toLowerCase()) {
             case "chrome":
-                WebDriverManager.chromedriver ().setup ();
-                driver = new ChromeDriver ( );
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
                 break;
             case "firefox":
-                WebDriverManager.firefoxdriver ().setup ();
-                driver = new FirefoxDriver ( );
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
                 break;
             default:
-                throw new RuntimeException ( "Invalid browser" );
+                throw new RuntimeException("Ivalid browser");
         }
-        driver.get ( ConfigsReader.getPropertyValue ( "url" ) );
-        //driver.manage ( ).window ( ).maximize ( );
-        driver.manage ( ).timeouts ( ).implicitlyWait (Constants.IMPLICIT_WAIT , TimeUnit.SECONDS );
-        PageInitializer.initializePageObjects ();
+        driver.get(ConfigsReader.getPropertyValue("url"));
+        //driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
+        PageInitializer.initializePageObjects();
     }
+
     /**
      * this method will close any open browser
      */
-    public static void tearDown ( ) {
+    public static void tearDown() {
         if (driver != null) {
-            driver.quit ( );
+            driver.quit();
         }
     }
 }
--- target/test-classes/features/login.feature
+++ target/test-classes/features/login.feature
@@ -1,35 +1,19 @@
 
-@featureTag  @login  # feature level tag
+@featureTag  @login # feature level tag
 Feature: Login Functionality
 
+  @validCreds @smoke # scenario level tag
+  Scenario: Login with valid credentials
+    #Given navigate to HRMS login page
+    When enter valid credentials
+    And click on login button
+    Then verify dashboard is displayed
+    #And quit the browser
 
-    @validCreds  # scenario level tag
-  Scenario: Login With Valid Credentials
-     # Given Navigate to HRMS login page
-    When Enter valid credentials
-    And Click on login button
-    Then Validate dashboard is displayed
-   # And Close browser
-
-     @smoke @invalidCreds @whatever  @regression  #  adding multiple scenario level tags
-   Scenario: Login With Invalid Credentials
-    # Given Navigate to HRMS login page
-     When Enter invalid credentials
-     And Click on login button
-     Then Validate error message equals expected result
-     #And Close browser
-
-  # Scenario:  Login with Empty Username
-   #  Given Navigate to HRMS login page
-    # When Leave an empty username
-  #   And Click on login button
-    # Then Validate error message for empty Username
-    # And Close browser
-
-  # Scenario: Login with Empty Password
-    # Given Navigate to HRMS login page
-   #  When Leave an empty password
-   #  And Click on login button
-   #  Then Validate error message for Empty Password
-   #  And Close browser
-
+  @smoke @syntax @regression @whatever # adding multiple scenario level tags
+  Scenario: Login with invalid credentials
+    #Given navigate to HRMS login page
+    When enter invalid credentials
+    And click on login button
+    Then verify error message
+    #And quit the browser
